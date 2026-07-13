d13057fca69237df8d372627e9410d12d6abf1f4
Chinwuba Onyeka Success <chisus2004@gmail.com>
2026-07-13 07:32:29 +0100

    First commit

---
+++ intro.js
@@ -0,0 +1,8 @@
+// intro.js
+const name = "Chinwuba Onyeka Success";
+const age = 22;
+const favoriteLanguage = "JavaScript";
+
+console.log(`Name: ${name}`);
+console.log(`Age: ${age}`);
+console.log(`Language: ${favoriteLanguage}`);
